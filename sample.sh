7cc80bf8f711969857a5800573cefa3dd5df3343
bdevops99 <bdevops99@gmail.com>
2023-04-18 14:33:37 +0530

    Echo usage

--- sample.sh
+++ sample.sh
@@ -1,3 +1,12 @@
 echo -e "\e[32mGreen Color"
 echo new line
-
+echo -e "\e[31mRed color\e[0m"
+echo 2nd line
+echo -e "\e[33mcolor"
+echo 3rd line
+echo -e "\e[34mcolor\e[0m"
+echo 4th line
+echo -e "\e[35m color"
+echo 5th line
+echo -e "\e[36m color\e[0m"
+echo 6th line
