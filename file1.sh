940457f1735272aabce231ae8da212291113e671
bdevops99 <bdevops99@gmail.com>
2023-04-18 21:13:28 +0530

    Variables usage

--- file1.sh
+++ file1.sh
@@ -1,6 +1,7 @@
-a=10
-name=devops
-echo a=$a
-echo name=${name}
-date=2023-04-18
-echo today date is ${date}
+#a=10
+#name=devops
+#echo a=$a
+#echo name=${name}
+# date=2023-04-18
+# echo today date is ${date}
+date=$(date +%F)
